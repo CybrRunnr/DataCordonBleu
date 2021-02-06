128704dab8e7f509b15541c88620b23dcc1af7cb
Alexander Nielson <nielale2@isu.edu>
2021-02-05 20:31:23 -0700

    Created classes for stuffing and unstuffing. Created forms for texts entry.

--- DataCordonBleu/Controllers/DecodeController.cs
+++ DataCordonBleu/Controllers/DecodeController.cs
@@ -3,11 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataCordonBleu.Models;
 
 namespace DataCordonBleu.Controllers {
     public class DecodeController : Controller {
         public IActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Unstuffer unstf) {
+            if (ModelState.IsValid) {
+
+            }
+            return View(unstf);
         }
     }
 }
--- DataCordonBleu/Controllers/EncodeController.cs
+++ DataCordonBleu/Controllers/EncodeController.cs
@@ -3,11 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataCordonBleu.Models;
 
 namespace DataCordonBleu.Controllers {
     public class EncodeController : Controller {
         public IActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Stuffer stf) {
+            if (ModelState.IsValid) {
+
+            }
+            return View(stf);
         }
     }
 }
