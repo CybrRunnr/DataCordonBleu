867f3f426846d428f4a50fb984c56cc8aed6e5fd
Alexander Nielson <nielale2@isu.edu>
2021-03-24 14:31:45 -0600

    Added test controller and code to see if removing color from an image is possible in ASP.NET

--- DataCordonBleu-Framework/Controllers/HomeController.cs
+++ DataCordonBleu-Framework/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace DataCordonBleu_Framework.Controllers {
     public class HomeController : Controller {
@@ -18,6 +20,28 @@ namespace DataCordonBleu_Framework.Controllers {
 
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Testing() {
+            string imgPath = @"C:\Web\DataCordonBleu\DataCordonBleu-Framework\Uploads\test.jpg";
+            string savePath = @"C:\Web\DataCordonBleu\DataCordonBleu-Framework\Exports\test.jpg";
+            Bitmap bmp = new Bitmap(imgPath);
+
+            //Gets X coordinate
+            for (int x = 0; x < bmp.Width; x++) {
+                //Gets X coordinate
+                for (int y = 0; y < bmp.Height; y++) {
+                    //Color: Struct that hold the ARGB values of a pixel
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(0, pixelColor.G, pixelColor.B);
+                    bmp.SetPixel(x, y, newColor);
+                }
+            }
+
+            //Image imgToSave;
+            bmp.Save(savePath, ImageFormat.Jpeg);
 
             return View();
         }
